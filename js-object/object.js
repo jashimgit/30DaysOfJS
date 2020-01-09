92f8a90eaf59e9b714cdbe884b90ba127c7bce0f
jashimgit <jashim.ncn@gmail.com>
2020-01-10 01:29:48 +0600

    inline function example added in js_function.html and FizzBuzz game quize

---
+++ js-object/object.js
@@ -0,0 +1,18 @@
+var breakfast = {
+    name : "The Lumberjack",
+    price : "$9.95",
+    ingreditents : ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+}
+console.log(breakfast);
+
+
+var sister = {
+    name: "Sarah",
+    age: 23,
+    parents: ["alice", "andy"],
+    siblings: ["julia"],
+    favoriteColor: "purple",
+    pets: true
+};
+
+console.log(sister);
