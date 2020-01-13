77fd7babfc25ae1d7ba6456eef1a448d3ce4768e
jashimgit <jashim.ncn@gmail.com>
2020-01-14 00:38:47 +0600

    experimenting array

--- challenge/array/app.js
+++ challenge/array/app.js
@@ -19,18 +19,33 @@ document.getElementById( 'demo' ).innerHTML = vegetables;
 vegetables.splice( 1, 0, 'Brocli' );
 document.getElementById( 'demo1' ).innerHTML = vegetables;
 // console.log( vegetables );
+
 vegetables.splice( -5, 1, 'Ginger' );
 document.getElementById( 'demo2' ).innerHTML = vegetables;
+
 vegetables.splice( -4, 1 );
 // print vegetables now
 document.getElementById( 'demo3' ).innerHTML = vegetables;
 
-console.log( vegetables );
+// console.log( vegetables );
 vegetables.pop()
-console.log( vegetables );
+// console.log( vegetables );
 vegetables.shift();
-console.log( vegetables );
+// console.log( vegetables );
 // access an item from an array say 'carrot'
 console.log( vegetables[ 2 ] );
-console.log(vegetables[vegetables.length -4]);
-document.getElementById( 'demo4' ).innerHTML = vegetables[2];
+// console.log( vegetables[ vegetables.length - 4 ] );
+document.getElementById( 'demo4' ).innerHTML = vegetables[ 2 ];
+// console.log( vegetables );
+// console.log( vegetables.indexOf( 'carrot' ) );
+console.log(vegetables.sort());
+
+value = [];
+for (let x = 0; x < 10; x++) {
+    value.push([
+        2 ** x, 2 * x ** 2
+    ])
+    
+}
+
+console.table(value);
---
+++ challenge/edabit.js
@@ -0,0 +1,18 @@
+/*
+problem 1: find the index (part 2)
+
+Create a function that searches
+for the index of a given item in an array.If the item is present, it should return the index, otherwise, it should
+return -1.
+
+search( [ 1, 2, 3, 4 ], 3 )➞ 2
+*/
+
+const arr = [ 5, 12, 8, 130, 44 ];
+
+
+function search(arr, item) {
+    const c = arr.indexOf('')
+}
+
+search(arr, 4);
