503dffeed4241cbbc659b17bf9567f5bd3539c1f
jashimgit <jashim.ncn@gmail.com>
2020-01-13 16:32:01 +0600

    todays README updated

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Three examples added in js_function.html, if anything need correction please giv
 Day00: 10 Jan, 2020
 ````` trying to solve some challenge from edabit.com.
 ````` I solved 10 problems of JavaScript, though all are very easy but a few are hard for me but I solved that.
+
+Days: 00 13 Jan, 2020
+````` How to work with Arrays in JavaScript
+        1. Array declation ( Associative Array);
+        2. Add an item to the end
+        3. Remove item from the end
+        4. add item in the front 
+        5. Remove item fron the front
+        6. How to use splice() method, and its power
+        7. How to access an array item.
--- challenge/array/app.js
+++ challenge/array/app.js
@@ -13,17 +13,24 @@
 // console.log( pushItem );
 // console.log( fruits );
 // Removes items from an index position
-let vagetables = [ 'Cabbage', 'Turnip', 'Radish', 'Carrot', 'Tomato', 'pumpkin' ];
-document.getElementById('demo').innerHTML = vagetables;
+let vegetables = [ 'Cabbage', 'Turnip', 'Radish', 'Carrot', 'Tomato', 'pumpkin' ];
+document.getElementById( 'demo' ).innerHTML = vegetables;
 
-console.log( vagetables );
+vegetables.splice( 1, 0, 'Brocli' );
+document.getElementById( 'demo1' ).innerHTML = vegetables;
+// console.log( vegetables );
+vegetables.splice( -5, 1, 'Ginger' );
+document.getElementById( 'demo2' ).innerHTML = vegetables;
+vegetables.splice( -4, 1 );
+// print vegetables now
+document.getElementById( 'demo3' ).innerHTML = vegetables;
 
-// let removedItems = vagetables.splice( -1, 1, 'califlower', 'Tomato' );
-// console.log( removedItems );
-// console.log( vagetables );
-console.log( vagetables.splice( 1, 0, 'Brocli' ) );
-document.getElementById('demo1').innerHTML = vagetables;
-console.log( vagetables );
-console.log( vagetables.splice( -5, 1, 'Ginger' ) );
-document.getElementById( 'demo2' ).innerHTML = vagetables;
-console.log( vagetables );
+console.log( vegetables );
+vegetables.pop()
+console.log( vegetables );
+vegetables.shift();
+console.log( vegetables );
+// access an item from an array say 'carrot'
+console.log( vegetables[ 2 ] );
+console.log(vegetables[vegetables.length -4]);
+document.getElementById( 'demo4' ).innerHTML = vegetables[2];
