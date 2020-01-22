cde37c0fd1bddcfd80bd3101f2fef426c755e8b2
jashimgit <jashim.ncn@gmail.com>
2020-01-22 15:08:37 +0600

    mini project two is now perfeclty shows all json data

--- mini_project/two/app.js
+++ mini_project/two/app.js
@@ -1,35 +1,36 @@
-var btn = document.getElementById("btn");
-var tableData = document.getElementById("dataList");
+var btn = document.getElementById( "btn" );
+var tableData = document.getElementById( "dataList" );
 
 
 
 // Add event to our button 
 // and process functionality 
-btn.addEventListener("click", function() {
+btn.addEventListener( "click", function () {
   var myRequest = new XMLHttpRequest();
-  myRequest.open("GET", "http://localhost/javascript/myJson_data.json");
-  myRequest.onload = function() {
+  myRequest.open( "GET", "http://localhost/javascript/myJson_data.json" );
+  myRequest.onload = function () {
     //   console.log();
-    var ourData = JSON.parse(myRequest.responseText);
-    renderHtml(ourData);
+    var ourData = JSON.parse( myRequest.responseText );
+    renderHtml( ourData );
   };
- 
-  myRequest.send();
-});
 
-function renderHtml(data) {
+  myRequest.send();
+} );
+
+function renderHtml( data ) {
   var htmlString = "";
 
-  for (let i = 0; i < data.length; i++) {
-        htmlString +=
-      "<tr><td>" + data[i].fname + "</td><td>" + data[i].lname +"</td><td>" +
-      data[i].city +"</td> <td>" +
-      data[i].address + "</td> <td>" +
-      data[i].zip + "</td></tr>";
+  for ( let i = 0; i < data.length; i++ ) {
+    htmlString += '<tr>';
+    htmlString += '<td>' + data[ i ].fname + '</td>';
+    htmlString += '<td>' + data[ i ].lname + '</td>';
+    htmlString += '<td>' + data[ i ].address.streetAddress + ', ' + data[ i ].address.city + ' ,' + data[ i ].address.state + '</td>';
+    htmlString += '<td>' + data[ i ].address.zip + '</td>';
+    htmlString += '</tr>';
 
-    
+
+
   } // end of 1st index loop
 
-  tableData.insertAdjacentHTML("beforeend", htmlString);
+  tableData.insertAdjacentHTML( "beforeend", htmlString );
 } // end of renderHtml 
- 
