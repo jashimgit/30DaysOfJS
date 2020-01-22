93e22dca72f443b22dd57ce387cfcc2e456a63d8
jashimgit <jashim.ncn@gmail.com>
2020-01-22 11:10:59 +0600

    app.js file error fixed in mini project two

--- mini_project/two/app.js
+++ mini_project/two/app.js
@@ -1,25 +1,19 @@
 var btn = document.getElementById("btn");
 var tableData = document.getElementById("dataList");
 
+
+
+// Add event to our button 
+// and process functionality 
 btn.addEventListener("click", function() {
   var myRequest = new XMLHttpRequest();
-
   myRequest.open("GET", "http://localhost/javascript/myJson_data.json");
-
-var tableData = document.getElementById("tableData");
-
-btn.addEventListener("click", function() {
-  var myRequest = new XMLHttpRequest();
-  
-  myRequest.open("GET", "http://localhost/javascript/myJson_data.json");
-  
   myRequest.onload = function() {
     //   console.log();
     var ourData = JSON.parse(myRequest.responseText);
     renderHtml(ourData);
   };
-
-  
+ 
   myRequest.send();
 });
 
@@ -27,15 +21,7 @@ function renderHtml(data) {
   var htmlString = "";
 
   for (let i = 0; i < data.length; i++) {
-    
-// testing loop for address object 
-  
-for (let ii = 0; ii < data[i].length; ii++) {
-  console.log(data[i].address.city[ii]);  
-}
-
-
-    htmlString +=
+        htmlString +=
       "<tr><td>" + data[i].fname + "</td><td>" + data[i].lname +"</td><td>" +
       data[i].city +"</td> <td>" +
       data[i].address + "</td> <td>" +
@@ -46,3 +32,4 @@ for (let ii = 0; ii < data[i].length; ii++) {
 
   tableData.insertAdjacentHTML("beforeend", htmlString);
 } // end of renderHtml 
+ 
